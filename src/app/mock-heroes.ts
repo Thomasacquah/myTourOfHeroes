15ecbabe8326a773dedfc307f39692be164cf87b
thomasacquah <tomacquah98@gmail.com>
2021-12-10 11:27:53 +0000

    added a horoes and message service

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 1, name: 'Thor' },
+  { id: 2, name: 'Iron Man' },
+  { id: 3, name: 'Hulk' },
+  { id: 4, name: 'Superman' },
+  { id: 5, name: 'Batman' },
+  { id: 6, name: 'Wonder Woman' },
+  { id: 7, name: 'Spiderman' },
+  { id: 8, name: 'Antman' },
+  { id: 9, name: 'Black Panther' },
+  { id: 10, name: 'Dr Strange' }
+];
